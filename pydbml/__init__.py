6eb19304040f4af59fa63f6748d84757e9811dfa
Vanderhoof <ddddsa@gmail.com>
2021-07-12 11:39:34 +0300

    simplify __repr__ and __str__ on all classes, add tests for them

--- pydbml/__init__.py
+++ pydbml/__init__.py
@@ -1,1 +1,9 @@
 from pydbml.parser import PyDBML, PyDBMLParseResults
+import unittest
+import doctest
+from . import classes
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(classes))
+    return tests
