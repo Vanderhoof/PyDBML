a4444e20c6ef7b0a875242704f9c57070be9f7d5
Vanderhoof <ddddsa@gmail.com>
2022-08-06 13:25:43 +0200

    Fix typing

--- pydbml/classes/reference.py
+++ pydbml/classes/reference.py
@@ -1,13 +1,12 @@
-from typing import Collection
-from typing import Literal
-from typing import List
-from typing import Optional
-from typing import TYPE_CHECKING
-from typing import Union
 from itertools import chain
+from typing import Collection
+from typing import List
+from typing import Literal
+from typing import Optional
+from typing import Union
 
-from pydbml.constants import MANY_TO_ONE
 from pydbml.constants import MANY_TO_MANY
+from pydbml.constants import MANY_TO_ONE
 from pydbml.constants import ONE_TO_MANY
 from pydbml.constants import ONE_TO_ONE
 from pydbml.exceptions import DBMLError
@@ -16,8 +15,6 @@ from pydbml.tools import comment_to_dbml
 from pydbml.tools import comment_to_sql
 from .base import SQLObject
 from .column import Column
-
-# if TYPE_CHECKING:  # pragma: no cover
 from .table import Table
 
 
@@ -51,12 +48,17 @@ class Reference(SQLObject):
     @property
     def join_table(self) -> Optional['Table']:
         if self.type != MANY_TO_MANY:
-            return
+            return None
+
+        if self.table1 is None:
+            raise TableNotFoundError(f"Cannot generate join table for {self}: table 1 is unknown")
+        if self.table2 is None:
+            raise TableNotFoundError(f"Cannot generate join table for {self}: table 2 is unknown")
 
         return Table(
             name=f'{self.table1.name}_{self.table2.name}',
             columns=(
-                Column(name=f'{c.table.name}_{c.name}', type=c.type, not_null=True, pk=True)
+                Column(name=f'{c.table.name}_{c.name}', type=c.type, not_null=True, pk=True)  # type: ignore
                 for c in chain(self.col1, self.col2)
             ),
             abstract=True
@@ -114,15 +116,14 @@ class Reference(SQLObject):
             raise DBMLError('Columns in col2 are from different tables')
 
     def _validate_for_sql(self):
-        if self.table1 is None:
-            raise TableNotFoundError('Table on col1 is not set')
-        if self.table2 is None:
-            raise TableNotFoundError('Table on col2 is not set')
+        for col in chain(self.col1, self.col2):
+            if col.table is None:
+                raise TableNotFoundError(f'Table on {col} is not set')
 
     def _generate_inline_sql(self, source_col: List['Column'], ref_col: List['Column']) -> str:
         result = comment_to_sql(self.comment) if self.comment else ''
         result += (
-            f'{{c}}FOREIGN KEY ({self._col_names(source_col)}) '
+            f'{{c}}FOREIGN KEY ({self._col_names(source_col)}) '  # type: ignore
             f'REFERENCES {ref_col[0].table._get_full_name_for_sql()} ({self._col_names(ref_col)})'
         )
         if self.on_update:
@@ -134,8 +135,9 @@ class Reference(SQLObject):
     def _generate_not_inline_sql(self, c1: List['Column'], c2: List['Column']):
         result = comment_to_sql(self.comment) if self.comment else ''
         result += (
-            f'ALTER TABLE {c1[0].table._get_full_name_for_sql()} ADD {{c}}FOREIGN KEY ({self._col_names(c1)}) '
-            f'REFERENCES {c2[0].table._get_full_name_for_sql()} ({self._col_names(c2)})'
+            f'ALTER TABLE {c1[0].table._get_full_name_for_sql()}'  # type: ignore
+            f' ADD {{c}}FOREIGN KEY ({self._col_names(c1)})'
+            f' REFERENCES {c2[0].table._get_full_name_for_sql()} ({self._col_names(c2)})'
         )
         if self.on_update:
             result += f' ON UPDATE {self.on_update.upper()}'
@@ -145,11 +147,11 @@ class Reference(SQLObject):
 
     def _generate_many_to_many_sql(self) -> str:
         join_table = self.join_table
-        table_sql = join_table.sql
+        table_sql = join_table.sql  # type: ignore
 
         n = len(self.col1)
-        ref1_sql = self._generate_not_inline_sql(join_table.columns[:n], self.col1)
-        ref2_sql = self._generate_not_inline_sql(join_table.columns[n:], self.col2)
+        ref1_sql = self._generate_not_inline_sql(join_table.columns[:n], self.col1)  # type: ignore
+        ref2_sql = self._generate_not_inline_sql(join_table.columns[n:], self.col2)  # type: ignore
 
         result = '\n\n'.join((table_sql, ref1_sql, ref2_sql))
         return result.format(c='')
@@ -195,7 +197,7 @@ class Reference(SQLObject):
             # settings are ignored for inline ref
             if len(self.col2) > 1:
                 raise DBMLError('Cannot render DBML: composite ref cannot be inline')
-            table_name = self.col2[0].table._get_full_name_for_sql()
+            table_name = self.col2[0].table._get_full_name_for_sql()  # type: ignore
             return f'ref: {self.type} {table_name}."{self.col2[0].name}"'
         else:
             result = comment_to_dbml(self.comment) if self.comment else ''
@@ -223,7 +225,7 @@ class Reference(SQLObject):
 
             options_str = f' [{", ".join(options)}]' if options else ''
             result += (
-                ' {\n    '
+                ' {\n    '  # type: ignore
                 f'{self.table1._get_full_name_for_sql()}.{col1} '
                 f'{self.type} '
                 f'{self.table2._get_full_name_for_sql()}.{col2}'
