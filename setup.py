3f83a6f21e71f87a2670ffa1705048f1fa734031
Vanderhoof <ddddsa@gmail.com>
2022-12-11 12:11:47 +0100

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    version='1.0.5',
+    version='1.0.6',
     author='Daniil Minukhin',
     author_email='ddddsa@gmail.com',
     url='https://github.com/Vanderhoof/PyDBML',
