e7d6a2a48616d5d96b7229a0af61bf31fa05244e
Vanderhoof <ddddsa@gmail.com>
2024-07-18 20:50:05 +0200

    dynamic options without tests yet

--- pydbml/renderer/dbml/default/note.py
+++ pydbml/renderer/dbml/default/note.py
@@ -1,24 +1,14 @@
-import re
 from textwrap import indent
 
 from pydbml.classes import Note
 from pydbml.renderer.dbml.default.renderer import DefaultDBMLRenderer
-
-
-def prepare_text_for_dbml(model):
-    '''Escape single quotes'''
-    pattern = re.compile(r"('''|')")
-    return pattern.sub(r'\\\1', model.text)
+from pydbml.renderer.dbml.default.utils import quote_string
 
 
 @DefaultDBMLRenderer.renderer_for(Note)
 def render_note(model: Note) -> str:
-    text = prepare_text_for_dbml(model)
-    if '\n' in text:
-        note_text = f"'''\n{text}\n'''"
-    else:
-        note_text = f"'{text}'"
+    text = quote_string(model.text)
 
-    note_text = indent(note_text, '    ')
-    result = f'Note {{\n{note_text}\n}}'
+    text = indent(text, '    ')
+    result = f'Note {{\n{text}\n}}'
     return result
--- pydbml/renderer/dbml/default/utils.py
+++ pydbml/renderer/dbml/default/utils.py
@@ -1,12 +1,26 @@
-from pydbml.renderer.dbml.default.note import prepare_text_for_dbml
+import re
+
 from pydbml.tools import comment
+
+
+def prepare_text_for_dbml(text: str) -> str:
+    '''Escape single quotes'''
+    pattern = re.compile(r"('''|')")
+    return pattern.sub(r'\\\1', text)
+
+
+def quote_string(text: str) -> str:
+    if '\n' in text:
+        return f"'''{prepare_text_for_dbml(text)}'''"
+    else:
+        return f"'{prepare_text_for_dbml(text)}'"
 
 
 def note_option_to_dbml(note: 'Note') -> str:
     if '\n' in note.text:
-        return f"note: '''{prepare_text_for_dbml(note)}'''"
+        return f"note: '''{prepare_text_for_dbml(note.text)}'''"
     else:
-        return f"note: '{prepare_text_for_dbml(note)}'"
+        return f"note: '{prepare_text_for_dbml(note.text)}'"
 
 
 def comment_to_dbml(val: str) -> str:
