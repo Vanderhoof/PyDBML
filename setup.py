2b2cbfb579449f4bb1587186309e6ecf6ff9af20
Vanderhoof <ddddsa@gmail.com>
2025-03-12 12:51:44 +0100

    update changelog and bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    version='1.1.4',
+    version='1.2.0',
     author='Daniil Minukhin',
     author_email='ddddsa@gmail.com',
     url='https://github.com/Vanderhoof/PyDBML',
