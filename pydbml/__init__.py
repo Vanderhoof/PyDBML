52d59ad2044f963aa34ba6ceca00180595705fa0
samhaese <63563702+samhaese@users.noreply.github.com>
2025-04-18 14:46:30 -0700

    Add load(s) and dump(s) methods

--- pydbml/__init__.py
+++ pydbml/__init__.py
@@ -1,3 +1,15 @@
+import os
+
 from . import _classes
 from .parser import PyDBML
 from .database import Database
+
+load = PyDBML.parse_file
+loads = PyDBML.parse
+
+def dump(db: Database, fp: str | os.PathLike):
+    with open(fp, 'w') as f:
+        f.write(db.dbml)
+
+def dumps(db: Database) -> str:
+    return db.dbml
